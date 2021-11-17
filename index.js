8d1786c87589fb590700dde8be8c1a0e88f5cdcb
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-11-17 14:29:37 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+let helperFunction = (collection) => {
+    return collection instanceof Array ? collection.slice() : Object.values(collection)
+}
+
+let myEach = (collection, callback) => {
+    let newCollection = helperFunction(collection)
+    for (let i = 0; i < newCollection.length; i++) {
+        callback(newCollection[i])
+    }
+    return collection
+}
+
+let myMap = (collection, callback) => {
+    let newAry = []
+    let newCollection = helperFunction(collection)
+    for (let i = 0; i < newCollection.length; i++) {
+        const result = callback(newCollection[i])
+        newAry.push(result)
+    }
+    return newAry
+}
