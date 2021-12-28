f8bf3596f3dc4ae6a39e7574b8deac7fa344f0b9
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-12-28 15:37:39 -0600

    Done.

--- index.js
+++ index.js
@@ -39,3 +39,47 @@ let myReduce = (collection, callback, acc) => {
     }
     return acc
 }
+
+// collection is an array of strings ["a", "b", "c"]
+// predicate is a callback function with a truthiness value (element) => element === "c"
+let myFind = (collection, predicate) => {
+    // console.log(collection)
+    for (let i = 0; i < collection.length; i++) {
+        let element = collection[i] 
+        // ((element) => element === "c")
+        if (predicate(element)) {
+            return element
+        }
+        // when iterating through collection, if value is equal to the value of the argument provided
+        // stop iteration and return that value
+        // else continue iterating through collection until value is found
+        // return undefined if the value is not present in collection
+    }
+    return undefined
+}
+
+// collection is an array of integers [1, 2, 3, 4, 5, 6]
+// predicate is a callback function with a truthiness value (element) => element % 2 == 0
+let myFilter = (collection, predicate) => {
+    let newAry = []
+    let newCollection = helperFunction(collection)
+    for (let i = 0; i < newCollection.length; i++) {
+        let element = newCollection[i]
+        if (predicate(element)) {
+            newAry.push(element)
+        }
+    }
+    return newAry
+}
+
+// collection is an array of integers or strings
+// return value is an integer resembling the number of values in the collection
+let mySize = (collection) => {
+    for (let i = 0; i < collection.length; i++) {
+        
+    //     // after the first iteration we want to return 1
+    //     // after each iteration following the first add 1
+    //     // after successfully iterating through the entire collection return the sum 
+    // }
+    }
+}
